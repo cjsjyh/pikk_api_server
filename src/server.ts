de963459f3dcf89a677e4760968cf1fab5941f1c
Junsoo Choi <cjsjyh@gmail.com>
2019-08-17 21:42:15 +0900

    Send image to AWS S3
    Able to send image to S3 and save

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,58 @@
+//IMPORT CORE PACKAGES
 import express from "express"
 import { ApolloServer } from "apollo-server-express"
-import depthLimit from "graphql-depth-limit"
 import { createServer } from "http"
 import compression from "compression"
 import cors from "cors"
+const path = require("path")
+
+//IMPORT GRAPHQL RELATED PACKAGES
+import depthLimit from "graphql-depth-limit"
 import schema from "./schema"
 
+//IMPORT S3
+import * as AWS from "aws-sdk"
+AWS.config.loadFromPath(path.join(__dirname, "/../config.json"))
+const S3 = new AWS.S3()
+
+//-------------------------------
+//TEMPORARY IMPORT FOR TESTING
+//-------------------------------
+import * as fs from "fs"
+
+//Create Express Server
 const app = express()
+app.use("*", cors())
+app.use(compression())
+
+//Create Apollo Server
 const server = new ApolloServer({
   schema,
   validationRules: [depthLimit(7)]
 })
-app.use("*", cors())
-app.use(compression())
+
+//Add graphql endpoint to Express
 server.applyMiddleware({ app, path: "/graphql" })
+
+app.get("/UploadImage", async (req, res) => {
+  var param = {
+    Bucket: "fashiondogam-images",
+    Key: "image/" + "testimage.jpg",
+    ACL: "public-read",
+    Body: fs.createReadStream(__dirname + "/../test.jpg"),
+    ContentType: "image/jpg"
+  }
+
+  S3.upload(param, function(err: Error, data: AWS.S3.ManagedUpload.SendData) {
+    if (err) {
+      console.log(err)
+    }
+    console.log(data)
+  })
+})
+
 const httpServer = createServer(app)
 httpServer.listen(
   { port: 3000 },
-  (): void =>
-    console.log(
-      `GraphQL is now running on http://localhost:3000/graphql`
-    )
+  (): void => console.log(`GraphQL is now running on http://localhost:3000/graphql`)
 )

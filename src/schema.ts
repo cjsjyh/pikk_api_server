6283d71fe3242d3a35bb7f05d6ef2a10731e8c99
Junsoo Choi <cjsjyh@gmail.com>
2019-08-17 16:57:55 +0900

    Fix Schema router

--- src/schema.ts
+++ src/schema.ts
@@ -2,11 +2,13 @@ import "graphql-import-node"
 import { makeExecutableSchema } from "graphql-tools"
 import { GraphQLSchema } from "graphql"
 import * as typeDefs from "./schema/schema.graphql"
-//import * as resolvers from "./resolvers/Query"
 const resolvers = require('./resolvers/resolvers')
+
 
 const schema: GraphQLSchema = makeExecutableSchema({
   typeDefs,
   resolvers
 })
+
+
 export default schema

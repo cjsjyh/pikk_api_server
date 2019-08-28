27a78a33f5f3aeb435d901d27366604e5f79488d
Junsoo Choi <jyhckb@naver.com>
2019-08-28 17:49:20 +0900

    RegisterItem done
    RegisterItem in Mutation.ts done.
    Inserts into Item

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,62 +1,107 @@
 //https://www.apollographql.com/docs/graphql-tools/resolvers/
 
-import { UserInfo } from "./Type"
-import { addCatchUndefinedToSchema } from "graphql-tools";
+import { UserInfo, ItemInfo } from "./Type"
+import { addCatchUndefinedToSchema } from "graphql-tools"
 const { pool } = require("../database/connectionPool")
 
-
 module.exports = {
-    helloWorld(parent: void, args: void): string {
-      return `👋 Hello world! 👋`
-    },
-    
-    RegisterUser: async (parent: Object, args: UserInfo): Promise<Boolean> => {
-        //Make Connection
-        let client;
-        try {
-            client = await pool.connect();
-        } catch(e){
-            console.log("[Error] Failed Connecting to DB")
-            return false
-        }
+  helloWorld(parent: void, args: void): string {
+    return `👋 Hello world! 👋`
+  },
 
-        //Make UserCredential
-        let id;
-        try {
-            let qResult = await client.query('INSERT INTO "USER_CONFIDENTIAL"("username","password") VALUES ($1,$2) RETURNING *',[args.username, args.password])
-            id = qResult.rows[0].id;
-        }catch(e){
-            console.log("[Error] Failed to Insert into USER_CONFIDENTIAL")
-            client.release()
-            return false;
-        }
-
-        //Make UserInfo
-        try {
-            let profileImgUrl = null
-            if(args.hasOwnProperty('profileImg'))
-            {
-                //Upload Image and retrieve URL
-            }
-            let qResult = await client.query('INSERT INTO "USER_INFO"("FK_accountId","name","email","age","height","weight","profileImg","phoneNum","address") VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',
-                    [id,args.name,args.email,args.age,args.height,args.weight,profileImgUrl,args.phoneNum,args.address])
-            console.log(qResult);
-            client.release()
-        } catch(e) {
-            console.log("[Error] Failed to Insert into User_Info")
-            console.log(e);
-            client.release()
-            return false
-        }
-
-        try {
-            let qResult = await client.query('INSERT INTO "CHANNEL"("FK_accountId") VALUES ($1)',[id])
-            return true;
-        }
-        catch(e){
-            return false;
-        }
+  RegisterUser: async (parent: Object, args: UserInfo): Promise<Boolean> => {
+    //Make Connection
+    let client
+    try {
+      client = await pool.connect()
+    } catch (e) {
+      console.log("[Error] Failed Connecting to DB")
+      return false
     }
 
+    //Make UserCredential
+    let id
+    try {
+      let qResult = await client.query(
+        'INSERT INTO "USER_CONFIDENTIAL"("username","password") VALUES ($1,$2) RETURNING *',
+        [args.username, args.password]
+      )
+      id = qResult.rows[0].id
+    } catch (e) {
+      console.log("[Error] Failed to Insert into USER_CONFIDENTIAL")
+      client.release()
+      return false
+    }
 
+    let profileImgUrl = null
+    if (args.hasOwnProperty("profileImg")) {
+      //Upload Image and retrieve URL
+    }
+
+    //Make UserInfo
+    try {
+      let qResult = await client.query(
+        'INSERT INTO "USER_INFO"("FK_accountId","name","email","age","height","weight","profileImg","phoneNum","address") VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',
+        [
+          id,
+          args.name,
+          args.email,
+          args.age,
+          args.height,
+          args.weight,
+          profileImgUrl,
+          args.phoneNum,
+          args.address
+        ]
+      )
+      client.release()
+    } catch (e) {
+      console.log("[Error] Failed to Insert into User_Info")
+      client.release()
+      return false
+    }
+
+    try {
+      await client.query('INSERT INTO "CHANNEL"("FK_accountId") VALUES ($1)', [id])
+      return true
+    } catch (e) {
+      console.log("[Error] Failed to Insert into Channel")
+      return false
+    }
+  },
+
+  RegisterItem: async (parent: void, args: ItemInfo): Promise<Boolean> => {
+    let client
+    try {
+      client = await pool.connect()
+    } catch (e) {
+      console.log("[Error] Failed Connecting to DB")
+      return false
+    }
+
+    let itemImgUrl = null
+    //Temporary//
+    itemImgUrl = "testURL"
+    //---------//
+    if (args.hasOwnProperty("itemImg")) {
+      //Upload Image and retrieve URL
+    }
+    if (!args.hasOwnProperty("args.currentPrice")) {
+      args.currentPrice = args.originalPrice
+    }
+
+    try {
+      await client.query(
+        'INSERT INTO "ITEM"("name","brand","originalPrice","currentPrice","itemType","imageUrl") VALUES ($1,$2,$3,$4,$5,$6)',
+        [args.name, args.brand, args.originalPrice, args.currentPrice, args.itemType, itemImgUrl]
+      )
+      return true
+    } catch (e) {
+      console.log("[Error] Failed to Insert into ITEM")
+      console.log(e)
+      return false
+    }
+
+    return true
   }
+}
--- src/resolvers/Type.ts
+++ src/resolvers/Type.ts
@@ -1,12 +1,21 @@
 export type UserInfo = {
-    username: string
-    password: string
-    name: string
-    email: string
-    age: number
-    height: number
-    weight: number
-    profileImg: File
-    phoneNum: number
-    address: string
+  username: string
+  password: string
+  name: string
+  email: string
+  age: number
+  height: number
+  weight: number
+  profileImg: File
+  phoneNum: number
+  address: string
+}
+
+export type ItemInfo = {
+  name: string
+  brand: string
+  originalPrice: number
+  currentPrice: number
+  itemType: string
+  itemImg: File
 }

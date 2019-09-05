95aeb4a71e59094b81eeec84e338966c13cb659b
Junsoo Choi <jyhckb@naver.com>
2019-09-05 17:44:23 +0900

    Add _Metadata
    For now, simply returns count of rows

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -32,6 +32,10 @@ module.exports = {
     }
   },
 
+  _allItemsMetadata: async (parent: void, args: QueryArgInfo): Promise<number> => {
+    return GetMetaData("ITEM")
+  },
+
   allCommunityPosts: async (parent: void, args: QueryArgInfo, ctx: void, info: GraphQLResolveInfo): Promise<[CustomType.CommunityPostInfo]> => {
     let arg: ArgType.CommunityPostQuery = args.communityPostOption
     let client
@@ -72,6 +76,10 @@ module.exports = {
     }
   },
 
+  _allCommunityPostsMetadata: async (parent: void, args: QueryArgInfo): Promise<number> => {
+    return GetMetaData("COMMUNITY_POST")
+  },
+
   allRecommendPosts: async (parent: void, args: QueryArgInfo, ctx: void, info: GraphQLResolveInfo): Promise<Boolean> => {
     let arg: ArgType.RecommendPostQuery = args.recommendPostOption
     let client
@@ -84,33 +92,38 @@ module.exports = {
     let sortSql = " ORDER BY " + arg.sortBy + " " + arg.filter.sort
     let limitSql = " LIMIT " + arg.filter.first + " OFFSET " + arg.filter.start
 
-    let postResult, imgResult: QueryResult
+    let postResult, reviewResult, cardResult
     try {
-      postResult = await client.query('SELECT * FROM "RECOMMEND_POST"' + sortSql + limitSql)
+      //postResult = await client.query('SELECT * FROM "RECOMMEND_POST"' + sortSql + limitSql)
+      console.log("POST============")
+      let postSql = 'SELECT * FROM "RECOMMEND_POST"' + sortSql + limitSql
+      postResult = await client.query(postSql)
+      postResult = postResult.rows
+      console.log(postResult)
+      //console.log(JSON.stringify(postResult, null, 4))
 
-      let PromiseResult: any = await Promise.all([
-        SequentialPromiseValue(postResult.rows, GetCommunityPostImage),
-        SequentialPromiseValue(postResult.rows, GetUserInfo)
-      ])
-      let imgResult: CustomType.ImageInfo[][] = PromiseResult[0]
-      let userResult: CustomType.UserInfo[] = PromiseResult[1]
+      console.log("REVIEW============")
+      let reviewSql = `WITH aaa AS (${postSql}) SELECT bbb.*, rank() OVER (PARTITION BY bbb."FK_postId") FROM "ITEM_REVIEW" AS bbb INNER JOIN aaa ON aaa.id = bbb."FK_postId"`
+      reviewResult = await client.query(reviewSql)
+      reviewResult = reviewResult.rows
+      console.log(reviewResult)
+      //console.log(JSON.stringify(reviewResult, null, 4))
 
-      postResult.rows.forEach((item: CustomType.RecommendPostInfo, index: number) => {
-        item.accountId = item.FK_accountId
-        item.name = userResult[index].name
-        item.profileImgUrl = userResult[index].profileImgUrl
-        /*
-        item.imageUrl = new Array()
-        imgResult[index].forEach(image => {
-          item.imageUrl.push(image.imageUrl)
-        })
-        */
-      })
+      console.log("CARD============")
+      let cardSql = `WITH aaa AS (${reviewSql}) SELECT bbb.*, rank() OVER (PARTITION BY bbb."FK_reviewId") FROM "ITEM_REVIEW_CARD" AS bbb INNER JOIN aaa ON aaa.id = bbb."FK_reviewId"`
+      cardResult = await client.query(cardSql)
+      cardResult = cardResult.rows
+      console.log(cardResult)
 
-      return postResult.rows
+      return reviewResult.rows
     } catch (e) {
+      console.log(e)
       throw new Error("[Error] Failed to fetch user data from DB")
     }
+  },
+
+  _allRecommendPostsMetadata: async (parent: void, args: QueryArgInfo): Promise<number> => {
+    return GetMetaData("RECOMMEND_POST")
   },
 
   getUser: async (parent: void, args: QueryArgInfo): Promise<[CustomType.UserInfo]> => {
@@ -177,22 +190,14 @@ function GetCommunityPostImage(postInfo: CustomType.CommunityPostInfo): Promise<
   })
 }
 
-function GetRecommendPostReview<T>(sql: string, parameters: Array<T>): Promise<QueryResult> {
-  return new Promise(async (resolve, reject) => {
-    let client
-    try {
-      client = await pool.connect()
-    } catch (e) {
-      throw new Error("[Error] Failed Connecting to DB")
-    }
+async function GetMetaData(tableName: string): Promise<number> {
+  let client
+  try {
+    client = await pool.connect()
+  } catch (e) {
+    throw new Error("[Error] Failed Connecting to DB")
+  }
 
-    try {
-      let queryResult = await client.query(sql, parameters)
-      client.release()
-      resolve(queryResult.rows)
-    } catch (e) {
-      client.release()
-      reject(e)
-    }
-  })
+  let countResult = await client.query(`SELECT COUNT(*) FROM "${tableName}"`)
+  return countResult.rows[0].count
 }

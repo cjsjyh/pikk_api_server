c0582711f0f6f34750c3567b1c5ac07906078b48
Junsoo Choi <jyhckb@naver.com>
2019-09-23 14:58:08 +0900

    Refactoring graphql types split into each function

--- src/schema.ts
+++ src/schema.ts
@@ -7,12 +7,7 @@ import { fileLoader, mergeTypes, mergeResolvers } from "merge-graphql-schemas"
 import * as fs from "fs"
 import * as glob from "glob"
 
-//const resolvers = require("./resolvers/index")
-
-//const graphqlArray = fileLoader(path.join(__dirname, "."), { recursive: true })
 const graphqlArray = fileLoader(path.join(__dirname, "/**/*.graphql"))
-//const graphqlArray = glob.sync(`${__dirname}/**/*.graphql`).map(x => fs.readFileSync(x, { encoding: "utf8" }))
-
 const resolverArray = fileLoader(path.join(__dirname, "./**/*.resolver.*"))
 
 const schema: GraphQLSchema = makeExecutableSchema({

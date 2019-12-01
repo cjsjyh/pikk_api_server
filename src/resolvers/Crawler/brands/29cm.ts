0b57a687a439bb3e1aad3c1acd16a09cb502c282
Junsoo Choi <jyhckb@naver.com>
2019-12-01 23:10:28 +0900

    [Chores] Check 29cm crawler

--- src/resolvers/Crawler/brands/29cm.ts
+++ src/resolvers/Crawler/brands/29cm.ts
@@ -5,43 +5,8 @@ import { strip, formatUrl } from "../../Utils/stringUtil"
 //var chrome = require("chromedriver")
 
 export async function crawl29cm(sourceUrl): Promise<CrawledItemInfo> {
-  let htmlCode
-  try {
-    var webdriver = require("selenium-webdriver")
+  let htmlCode = await getHtmlRequest(sourceUrl)
 
-    //#Option 1
-    // var chromeCapabilities = webdriver.Capabilities.chrome()
-    // //setting chrome options to start the browser fully maximized
-    // var chromeOptions = {
-    //   args: ["--headless", "--disable-gpu", "--no-sandbox", "--disable-extensions", "--disable-dev-shm-usage"]
-    // }
-    // chromeCapabilities.set("chromeOptions", chromeOptions)
-    // var driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build()
-
-    //#Option 2
-    // var options = new chrome.Options()
-    // options.addArguments(["--headless", "lang=ko_KR", "--no-sandbox", "--disable-dev-shm-usage"])
-    // var driver = new webdriver.Builder().withCapabilities(options.toCapabilities()).build()
-
-    //var chrome = require("selenium-webdriver/chrome")
-    // process.env["PATH"] += "/mnt/c/users/junsoo/desktop/chromedriver"
-    // var options = new chrome.Options()
-    // var driver = new webdriver.Builder().withCapabilities(options.toCapabilities()).build()
-
-    // var chrome = require("selenium-webdriver/chrome")
-    // var driver = new chrome.Driver()
-    var driver = new webdriver.Builder().forBrowser("chrome").build()
-
-    //var driver = new webdriver.Builder().forBrowser("chrome").build()
-    await driver.get(sourceUrl)
-    htmlCode = driver.getPageSource()
-  } catch (e) {
-    console.log(e)
-    return null
-  }
-
-  //let htmlCode = await getHtmlRequest(sourceUrl)
-  console.log(htmlCode)
   //Mistyped class name
   let brand = parseHtml(htmlCode, "string", "value", ".brnad_link", "_ngcontent-c36")
   let price = parseHtml(htmlCode, "number", "value", ".o", ".num")
